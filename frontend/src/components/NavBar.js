c5d2e157be1d669fa1b8bf85aab86295603f8490
Jhanavi Thakkar <jhanavi.a.thakkar@vanderbilt.edu>
2024-02-02 13:33:48 -0600

    responsive nav bar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,27 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logoLeft from "../assets/Vanderbilt_University_logo.png";
 import logoRight from "../assets/logo.png";
 import "./NavBar.css"
 
 const Navbar = () => {
-    return (
-      <div className="navbar">
-        <div className="left-section">
+    const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+  return (
+    <div className={`navbar ${menuOpen ? "open" : ""}`}>
+      <div className="left-section">
         <Link to="/home">
           <img src={logoLeft} alt="Left Logo" className="logo-left" />
         </Link>
-          <div className="links">
-            <Link to="/restaurant">Restaurant</Link>
-            <Link to="/blogs">Blogs</Link>
-            <Link to="/profile">Profile</Link>
-          </div>
-        </div>
-        <div className="right-section">
-          <img src={logoRight} alt="Right Logo" className="logo-right" />
+        <div className={`links ${menuOpen ? "show" : ""}`}>
+          <Link to="/restaurant">Restaurant</Link>
+          <Link to="/blogs">Blogs</Link>
+          <Link to="/profile">Profile</Link>
         </div>
       </div>
-    );
-  };
-  
-  export default Navbar;
+      <div className="right-section">
+        <img src={logoRight} alt="Right Logo" className="logo-right" />
+      </div>
+      <div className="menu-icon" onClick={toggleMenu}>
+        &#9776;
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
