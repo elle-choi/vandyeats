d9f2da94722e08db82785ce938b366af0e399696
Jhanavi Thakkar <jhanavi.a.thakkar@vanderbilt.edu>
2024-02-22 15:12:49 -0600

    create and view blog posts connected to firebase

---
+++ frontend/src/assets/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const colorTheme = {
+    theme: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'green': '#38761D',
+        'green_hover': '#2b5916'
+      },
+    },
+  };
+  
+  module.exports = colorTheme;
+  
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,6 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from 'firebase/storage';
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -26,3 +28,4 @@ const analytics = getAnalytics(app);
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
