e1236ff6dcb52a31ef0b005aad0aa3cdf43b45cb
elle924 <jeong.eun.choi@vanderbilt.edu>
2024-04-10 11:43:17 -0500

    profile pic, average rating, num posts

--- frontend/src/components/RestaurantInfo.jsx
+++ frontend/src/components/RestaurantInfo.jsx
@@ -57,19 +57,31 @@ const RestaurantInfo = () => {
 
     /*Restaurant Info Separation*/ 
     const basic_text = [" |",""]
-    const [review_cnt,setReview_cnt] = useState([]);
-    const [avg,setAvg] = useState([]);
     
 
     
     const navigate = useNavigate();
     const [postList, setPostList] = useState([]);
     const postsCollectionRef = collection(db, "posts");
+    const [userProfilePics, setUserProfilePics] = useState({});
 
     useEffect(() => {
         const getPosts = async () => {
             try {
                 const data = await getDocs(postsCollectionRef);
+                
+                const usersCollectionRef = collection(db, "users");
+                const usersSnapshot = await getDocs(usersCollectionRef);
+                const userProfilePicsData = {};
+        
+                usersSnapshot.forEach((doc) => {
+                    const userData = doc.data();
+                    userProfilePicsData[doc.id] = userData.profilePic;
+                });
+        
+                setUserProfilePics(userProfilePicsData);
+                
+
                 const posts = data.docs.map(doc => ({ ...doc.data(), id: doc.id }));
 
                 
@@ -88,15 +100,16 @@ const RestaurantInfo = () => {
                     }
                 }));
 
-                setPostList(postsWithUserImages);
-                setReview_cnt(postsWithUserImages.length);
-                const total =  postsWithUserImages.reduce((acc,post) => acc + ( post.rating || 0 ),0);
-                setAvg(postsWithUserImages.length>0?total/postsWithUserImages.length:0);
+
+                setPostList(postsWithUserImages);    
             } catch (error) {
-                console.error("Error fetching posts:", error);
-            }
+              console.error("Error fetching user profile pictures:", error);
+                
+                
+            } 
         };
 
+ 
         getPosts();
     }, []);
 
@@ -125,8 +138,9 @@ const RestaurantInfo = () => {
                         <div class='flex items-center'>
                         <svg class='w-5 h-5 mr-1' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.88 117.42"><path d="M66.71 3.55L81.1 37.26l36.58 3.28v-.01c1.55.13 2.91.89 3.85 2.01a5.663 5.663 0 011.32 4.13v.01a5.673 5.673 0 01-1.69 3.57c-.12.13-.25.25-.39.36L93.25 74.64l8.19 35.83c.35 1.53.05 3.06-.73 4.29a5.652 5.652 0 01-3.54 2.52l-.14.03c-.71.14-1.43.15-2.12.02v.01c-.75-.13-1.47-.42-2.11-.84l-.05-.03-31.3-18.71-31.55 18.86a5.664 5.664 0 01-7.79-1.96c-.38-.64-.62-1.33-.73-2.02-.1-.63-.09-1.27.02-1.89.02-.13.04-.27.08-.4l8.16-35.7c-9.24-8.07-18.74-16.1-27.83-24.3l-.08-.08a5.64 5.64 0 01-1.72-3.7c-.1-1.45.36-2.93 1.4-4.12l.12-.13.08-.08a5.668 5.668 0 013.77-1.72h.06l36.34-3.26 14.44-33.8c.61-1.44 1.76-2.5 3.11-3.05 1.35-.54 2.9-.57 4.34.04.69.29 1.3.71 1.8 1.22.53.53.94 1.15 1.22 1.82l.02.06zm10.19 37.2L61.85 5.51a.42.42 0 00-.09-.14.42.42 0 00-.14-.09.427.427 0 00-.35 0c-.1.04-.19.12-.24.24L45.98 40.75c-.37.86-1.18 1.49-2.18 1.58l-37.9 3.4c-.08.01-.16.02-.24.02-.06 0-.13.02-.18.05-.03.01-.05.03-.07.05l-.1.12c-.05.08-.07.17-.06.26.01.09.04.18.09.25.06.05.13.11.19.17l28.63 25c.77.61 1.17 1.62.94 2.65l-8.51 37.22-.03.14c-.01.06-.02.12-.01.17a.454.454 0 00.33.36c.12.03.24.02.34-.04l32.85-19.64c.8-.5 1.85-.54 2.72-.02L95.43 112c.08.04.16.09.24.14.05.03.1.05.16.06v.01c.04.01.09.01.14 0l.04-.01c.12-.03.22-.1.28-.2.06-.09.08-.21.05-.33L87.8 74.28a2.6 2.6 0 01.83-2.55l28.86-25.2c.04-.03.07-.08.1-.13.02-.04.03-.1.04-.17a.497.497 0 00-.09-.33.48.48 0 00-.3-.15v-.01c-.01 0-.03 0-.03-.01l-37.97-3.41c-1-.01-1.93-.6-2.34-1.57z" fill="#000000"/></svg>
                             <div class='flex'>
-                                <p class='font-bold'>{avg}</p> 
-                                <p>(+{review_cnt} Blogs) |</p>
+                                <p class='font-bold'>{parseFloat(postList.filter(post=>post.restaurant===names[place_id]).length > 0 ? (postList.filter(post=>post.restaurant===names[place_id]).reduce((acc,post) => acc + ( post.rating || 0 ),0)/postList.filter(post=>post.restaurant===names[place_id]).length):0).toFixed(2)}</p> 
+                                {postList.filter(post=>post.restaurant===names[place_id]).length < 100 && <p>({postList.filter(post=>post.restaurant===names[place_id]).length} Blogs) |</p>}
+                                {postList.filter(post=>post.restaurant===names[place_id]).length >= 100 &&  <p>(+(100+) Blogs) |</p>}
                             </div>
                            
                             {place_info[place_id].map((item, index) => (
@@ -153,7 +167,11 @@ const RestaurantInfo = () => {
                             <div className="flex-col w-10/12">
                                 <div class='flex items-center'>
                                     {/* INSERT PROFILE IMAGE CODE HERE */}
-                                    <div class='bg-slate-400 h-12 w-12 rounded-full mr-3' ></ div>
+                                    <img
+                                        src={userProfilePics[post.author.id]}
+                                        alt={`Profile of ${post.author.name}`}
+                                        class="rounded-full mr-3 w-14 h-14"
+                                    />
                                     <p className="text-green-600">{post.author.name}</p>
                                 </div>
                                 
