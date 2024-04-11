998e8ab8c93f8a5f7b5753e98bf07162a8b96318
Jhanavi Thakkar <jhanavi.a.thakkar@vanderbilt.edu>
2024-04-10 22:40:24 -0500

    text overflow issue fix

--- frontend/src/components/Blogs/CreatePost.jsx
+++ frontend/src/components/Blogs/CreatePost.jsx
@@ -200,43 +200,44 @@ const CreatePost = () => {
               <div className="review-input">
               <label htmlFor="review">Review</label>
               <ReactQuill
-                id="review"
-                value={review}
-                onChange={setReview}
-                className="quill-editor"
-                modules={{
-                  toolbar: [
-                    [{ header: "1" }, { header: "2" }, { font: [] }],
-                    [{ size: [] }],
-                    ["bold", "italic", "underline", "strike", "blockquote"],
-                    [
-                      { list: "ordered" },
-                      { list: "bullet" },
-                      { indent: "-1" },
-                      { indent: "+1" },
-                    ],
-                    ["link", "image", "video"],
-                    ["clean"],
-                  ],
-                }}
-                formats={[
-                  "header",
-                  "font",
-                  "size",
-                  "bold",
-                  "italic",
-                  "underline",
-                  "strike",
-                  "blockquote",
-                  "list",
-                  "bullet",
-                  "indent",
-                  "link",
-                  "image",
-                  "video",
-                ]}
-                placeholder="Write your review here..."
-              />
+  id="review"
+  value={review}
+  onChange={setReview}
+  className={`quill-editor quill-editor-wrapper`} 
+  modules={{
+    toolbar: [
+      [{ header: "1" }, { header: "2" }, { font: [] }],
+      [{ size: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image", "video"],
+      ["clean"],
+    ],
+  }}
+  formats={[
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "video",
+  ]}
+  placeholder="Write your review here..."
+/>
+
             </div>
             </div>
             
